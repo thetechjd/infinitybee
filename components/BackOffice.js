46d89af23e74c1bc478708e1d0567402aa52e7cd
Costel <cbarusc@yahoo.com>
2023-07-04 13:46:23 +0300

    claim show time v2

--- components/BackOffice.js
+++ components/BackOffice.js
@@ -320,11 +320,11 @@ export default function BackOffice({
      })
      
      useEffect(() => {
-        //  const interval = setInterval(() => {
+         const interval = setInterval(() => {
      
-        //    setClaimTime(claimTime => claimTime - 1);
-        //  }, 1000);
-        //  return () => clearInterval(interval);
+           setClaimTime(claimTime => claimTime - 1);
+         }, 1000);
+         return () => clearInterval(interval);
      
        }, [])
 
@@ -804,9 +804,9 @@ export default function BackOffice({
         var m = Math.floor(seconds % 3600 / 60);
         var s = Math.floor(seconds % 60);
     
-        var dDisplay = d > 0 ? d + ":" : "";
-        var hDisplay = h > 0 ? (h < 10 ? "0" + h + ":" : h + ":") : "";
-        var mDisplay = m > 0 ? (m < 10 ? "0" + m + ":" : m + ":") : "";
+        var dDisplay = d > 0 ? d : "";
+        var hDisplay = h > 0 ? (h < 10 ? "0" + h + ":" : h ) : "";
+        var mDisplay = m > 0 ? (m < 10 ? "0" + m + ":" : m ) : "";
         var sDisplay = s > 0 ? (s < 10 ? "0" + s : s) : "";
     
         
